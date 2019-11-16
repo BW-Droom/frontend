eb18f081f6b3048ccf2950604afcdf3c0f12175a
dangus1924 <51095136+dangus1924@users.noreply.github.com>
2019-11-15 20:47:12 -0500

    moved everything to the root

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+// react router dom
+import { BrowserRouter as Router } from 'react-router-dom'
+//redux 
+import { Provider } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+// redux store
+const store = createStore( applyMiddleware(thunk, logger))
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>
+, document.getElementById('root'));
+
