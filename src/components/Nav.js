63cf846814e42d63a2d60939e9d53694855777a0
Dallas James <dallasjames42@gmail.com>
2019-11-20 18:27:42 -0700

    merging

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -9,15 +9,14 @@ import JobSearch from './JobSeeker/JobSeeking';
 import Account from './Account';
 import { getToken } from '../utils/api';
 import ProtectedRoute from '../utils/ProtectedRoute';
-import SearchEmployees from './Company/NewEmployeeSearch';
 import SeekerForm from './JobSeeker/JobSeekerForm';
 import MainPage from './MainPage';
 import Logout from './logout';
 
 function Nav(props) {
     const signedIn = getToken()
-    const seeker = props.type === 'employee'
-    const company = props.type === 'employer'
+    // const seeker = props.type === 'employee'
+    // const company = props.type === 'employer'
 
     return(
         <>
@@ -27,16 +26,15 @@ function Nav(props) {
           <p className='slogan'>Swipe for your check</p>
         </div>
         <nav>
-            <Link to='/'>Home</Link>
-            <Link to='/signin'>Sign in</Link>
-            <Link to='/signup'>Sign up</Link>
-            {seeker && signedIn && <Link to='/seeker/dashboard'>Job-Seeker Swiping</Link>}
-            {seeker && signedIn && <Link to='/seeker/search'>Search for Jobs</Link>}
-            {seeker && signedIn && <Link to='/seeker/account'>Account</Link>}
-            {seeker && signedIn && <Link to='/seeker/dreamjob'>Create a Listing</Link>}
-            {company && signedIn && <Link to='/company/search'>Search for Employees</Link>}
-            {company && signedIn && <Link to='/company/dashboard'>Company Swiping</Link>}
-            {company && signedIn && <Link to='/company/listing'>Create a Listing</Link>}
+            {!signedIn && <Link to='/'>Home</Link>}
+            {!signedIn && <Link to='/signin'>Sign in</Link>}
+            {!signedIn && <Link to='/signup'>Sign up</Link>}
+            {signedIn && <Link to='/seeker/dashboard'>Job-Seeker Swiping</Link>}
+            {signedIn && <Link to='/seeker/search'>Search for Jobs</Link>}
+            {signedIn && <Link to='/seeker/account'>Account</Link>}
+            {signedIn && <Link to='/seeker/dreamjob'>Dream Job</Link>}
+            {signedIn && <Link to='/company/dashboard'>Company Swiping</Link>}
+            {signedIn && <Link to='/company/listing'>Create a Listing</Link>}
             {signedIn && <Link to='/logout'>Log Out</Link>}
         </nav>
         </div>
@@ -47,7 +45,6 @@ function Nav(props) {
         <ProtectedRoute exact path='/seeker/search' component={JobSearch} />
         <ProtectedRoute exact path='/seeker/account' component={Account} />
         <ProtectedRoute exact path='/seeker/dreamjob' component={SeekerForm} />
-        <ProtectedRoute exact path='/company/search' component={SearchEmployees} />
         <ProtectedRoute exact path='/company/dashboard' component={CompanySwiping} />
         <ProtectedRoute exact path='/company/listing' component={ListingForm} />
         <ProtectedRoute exact path='/logout' component={Logout} />
