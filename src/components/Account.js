8d38276e62157c650c087159fb3fa945c2541d94
ChiragThesia <harithesia@gmail.com>
2019-11-22 08:25:26 +0300

    Dashboard for both users set up. Need to plug data

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+33import React, { useState, useEffect } from 'react';
 import api from '../utils/api';
 
 function Account(props) {
@@ -28,14 +28,14 @@ function Account(props) {
             })
     }, [])
     return(
-        <main>
+        <>
         <h3>Name: {userProfile.name}</h3>
         <h3>Age: {userProfile.age}</h3>
         <h3>Occupation: {userProfile.occupation}</h3>
         <h3>Interests: {userProfile.droom}</h3>
         <h3>Dream Job: Software Developer</h3>
         <button>Edit</button>
-        </main>
+        </>
     )
 }
 
