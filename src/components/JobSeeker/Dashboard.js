23d28c80a21376540fc79869322f2bccb9b29872
ChiragThesia <harithesia@gmail.com>
2019-11-22 08:28:45 +0300

    Dashboard for both users set up. Need to plug data

--- src/components/JobSeeker/Dashboard.js
+++ src/components/JobSeeker/Dashboard.js
@@ -1,153 +1,126 @@
-import React from "react";
+import React from 'react';
 import clsx from "clsx";
 import { makeStyles } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import IconButton from "@material-ui/core/IconButton";
-import Badge from "@material-ui/core/Badge";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 
+import Account from '../Account';
 
+// Company swipes thru job seekers
+  
 //Styling for dashboard content.
 
 const drawerWidth = 240;
 
 const useStyles = makeStyles(theme => ({
-  root: {
+    root: {
     display: "flex"
-  },
-  toolbar: {
+    },
+    toolbar: {
     paddingRight: 24 // keep right padding when drawer closed
-  },
-  toolbarIcon: {
+
+    },
+    toolbarIcon: {
     display: "flex",
     alignItems: "center",
     justifyContent: "flex-end",
     padding: "0 8px",
     ...theme.mixins.toolbar
-  },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(["width", "margin"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
-    })
-  },
-  appBarShift: {
+    },
+   
+    appBarShift: {
     marginLeft: drawerWidth,
     width: `calc(100% - ${drawerWidth}px)`,
     transition: theme.transitions.create(["width", "margin"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen
     })
-  },
-  menuButton: {
+    },
+    menuButton: {
     marginRight: 36
-  },
-  menuButtonHidden: {
+    },
+    menuButtonHidden: {
     display: "none"
-  },
-  title: {
+    },
+    title: {
     flexGrow: 1
-  },
-  drawerPaper: {
+    },
+    drawerPaper: {
     position: "relative",
     whiteSpace: "nowrap",
     width: drawerWidth,
     transition: theme.transitions.create("width", {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen
     })
-  },
-  drawerPaperClose: {
+    },
+    drawerPaperClose: {
     overflowX: "hidden",
     transition: theme.transitions.create("width", {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen
     }),
     width: theme.spacing(7),
     [theme.breakpoints.up("sm")]: {
-      width: theme.spacing(9)
+        width: theme.spacing(9)
     }
-  },
-  appBarSpacer: theme.mixins.toolbar,
-  content: {
+    },
+    appBarSpacer: theme.mixins.toolbar,
+    content: {
     flexGrow: 1,
     height: "100vh",
     overflow: "auto"
-  },
-  container: {
+    },
+    container: {
     paddingTop: theme.spacing(4),
     paddingBottom: theme.spacing(4)
-  },
-  paper: {
+    },
+    paper: {
     padding: theme.spacing(2),
     display: "flex",
     overflow: "auto",
     flexDirection: "column"
-  },
-  fixedHeight: {
+    },
+    fixedHeight: {
     height: 240
-  }
+    }
 }));
 
 //the Dashboard Component relies here.
-function Dashboard() {
-  const classes = useStyles();
+function JobSeekerSwiping(){
+    const classes = useStyles();
 
-  const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
+    const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
 
-  return (
+    return (
     <div className={classes.root}>
-      <CssBaseline />
-      <AppBar position="absolute">
-        <Toolbar>
-          <Typography
-            component="h1"
-            variant="h6"
-            color="inherit"
-            noWrap
-            className={classes.title}
-          >
-            Job Seeker Dashboard
-          </Typography>
-          <IconButton color="inherit">
-            
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-
-      <main className={classes.content}>
-        <div className={classes.appBarSpacer} />
+        <CssBaseline />
+        
         <Container maxWidth="lg" className={classes.container}>
-          <Grid container spacing={3}>
+            <Grid container spacing={3}>
             <Grid item xs={12} md={8} lg={9}>
-              <Paper className={fixedHeightPaper}>
+                <Paper className={fixedHeightPaper}>
                 <ui>
-                  <li>Name: </li>
-                  <li>Favorite Job: </li>
-                  <li>Current Job: </li>
-                  <li>Favorite Place to work: </li>
+                    <Account/>
                 </ui>
-              </Paper>
+                </Paper>
             </Grid>
             {/* Recent Deposits */}
             <Grid item xs={12} md={4} lg={3}>
-              <Paper className={fixedHeightPaper}>User Data</Paper>
+                <Paper className={fixedHeightPaper}>User Data</Paper>
             </Grid>
             {/* Recent Orders */}
             <Grid item xs={12}>
-              <Paper className={classes.paper}>Current Matches?</Paper>
+                <Paper className={classes.paper}>Current Matches?</Paper>
             </Grid>
-          </Grid>
+            </Grid>
         </Container>
-      </main>
     </div>
-  );
+    );
 }
+    
 
-export default Dashboard;
+
+export default JobSeekerSwiping
