a3426e767513e89281e3be6655a5051fffd2ed77
dangus1924 <51095136+dangus1924@users.noreply.github.com>
2019-11-24 18:15:12 -0500

    merge

--- src/components/Company/Job/Listings.js
+++ src/components/Company/Job/Listings.js
@@ -3,22 +3,22 @@ import { connect } from 'react-redux';
 import { deleteJob } from '../../store/actions/index';
 import Job from './Job';
 
-function List(props) {  
-    const jobs = props.jobs;
-    const listItems = jobs.map((job) =>
-      <Job
-        job={job}
-        key={job.id}
-        deleteJob={props.deleteJob.bind(null, job.id)}
-        
-      />
-      )
+function List(props) { 
+  console.log(props) 
+    
+     
       
       
   return (
-    <>
-      {listItems}
-    
+    <>     
+      
+    {props.jobs.map(job => (
+       <Job
+       job={job}
+       key={job.id}
+       deleteJob={props.deleteJob.bind(null, job.id)}
+     /> 
+     ))}   
       
     </>
   );
